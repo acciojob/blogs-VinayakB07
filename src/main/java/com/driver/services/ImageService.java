dc3cb14c0f1985e36906b58bbf073806f265402f
Vinayak B <vinayakbalachandra07@gmail.com>
2023-02-18 14:12:21 +0530

    dont know

--- src/main/java/com/driver/services/ImageService.java
+++ src/main/java/com/driver/services/ImageService.java
@@ -17,15 +17,32 @@ public class ImageService {
 
     public Image addImage(Integer blogId, String description, String dimensions){
         //add an image to the blog
+        Blog blog=blogRepository2.findById(blogId).get();
+        Image image=new Image();
+        image.setDescription(description);
+        image.setDimension(dimensions);
+        image.setBlog(blog);
+
+        List<Image>imageList=blog.getImageList();
+        imageList.add(image);
+        blog.setImageList(imageList);
+
+        blogRepository2.save(blog);
+        return image;
 
     }
 
     public void deleteImage(Integer id){
-
+        imageRepository2.deleteById(id);
     }
 
     public int countImagesInScreen(Integer id, String screenDimensions) {
         //Find the number of images of given dimensions that can fit in a screen having `screenDimensions`
-
+        Image image=imageRepository2.findById(id).get();
+        String imageDimension=image.getDimension();
+        String s1[]=imageDimension.split("X");
+        String s2[]=screenDimensions.split("X");
+        int imageInScreen=(Integer.valueOf(s2[0])*Integer.valueOf(s2[1]))/(Integer.valueOf(s1[0])*Integer.valueOf(s1[0]));
+        return imageInScreen;
     }
 }
